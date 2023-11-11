1c34d1bc5355edb5b893a3f51d40639df3eaa9b0
JoseAntonioGalan <galanmaquedajoseantonio@gmail.com>
2023-11-11 22:02:29 +0100

    Eliminacion imports innecesarios

--- src/main/java/com/configuraciones/api/repositories/ConfigParamRepository.java
+++ src/main/java/com/configuraciones/api/repositories/ConfigParamRepository.java
@@ -1,7 +1,6 @@
 package com.configuraciones.api.repositories;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
